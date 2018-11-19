3862d819db186215ca0f94c6a8587860ff2aa296
Misael Jonathan <misaeljonathan17@gmail.com>
2018-11-19 18:52:43 +0700

    added html

---
+++ main.js
@@ -0,0 +1,7 @@
+function showPiece(event) {
+    console.log(event);
+    var sael = document.getElementById(event);
+    1;
+    console.log('sael', sael);
+    sael.classList.add("crown-white");
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+
+function showPiece(event) {
+    console.log(event);
+    const sael = document.getElementById(event);1
+    console.log('sael', sael);
+
+    sael.classList.add("crown-white");
+}
