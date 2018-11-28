905a0d558fd75ec8b8a0d23d3a5603e9de97eeec
kevinprakasa <kevinprakasa03@gmail.com>
2018-11-28 17:29:32 +0700

    change style

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-
-function showPiece(event) {
-    console.log(event);
-    const sael = document.getElementById(event);1
-    console.log('sael', sael);
-
-    sael.classList.add("crown-white");
+var turnCounter = 0;
+console.log(turnCounter);
+function clickedPiece(event) {
+    var mod_4 = turnCounter % 4;
+    const element = document.getElementById(event);
+    console.log(mod_4,turnCounter);
+    switch (mod_4) {
+        case 0:
+            element.classList.add("crown-red");
+            break;
+        case 1:
+            element.classList.add("crown-blue");
+            break;
+        case 2:
+            element.classList.add("helmet-red");
+            break;
+        case 3:
+            element.classList.add("helmet-blue");
+            break;
+        default:
+            break;
+    }
+    turnCounter++;
 }
